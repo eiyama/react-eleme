89129e679176cac864831c6a3c0f5b3c0b297284
lizi <1542469956@qq.com>
2018-12-01 11:39:48 +0800

    swiper-banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ import 'whatwg-fetch'
 import './home.scss'
 import Filter from '../../common/filter/Filter.jsx'
 import nav_img from '../../assets/nav.jpg'
+import yh_img from '../../assets/yh.jpg'
+import banner_img from '../../assets/banner.jpg'
+
 
 import Swiper from 'swiper/dist/js/swiper.js'
 import 'swiper/dist/css/swiper.min.css'
@@ -13,12 +16,14 @@ export default class Home extends Component{
         super();
         this.state={
             navList:[],
+            yhList:[],
+            bannerList:[],
             channelsData:[]
         }
     }
 
     render(){
-        let {navList,channelsData} = this.state;
+        let {navList,yhList,bannerList,channelsData} = this.state;
         return (
             <div className="page" id="home">
                 <div className="wrap">
@@ -40,8 +45,7 @@ export default class Home extends Component{
                     <div className="nav-container swiper-container">
                         <div className="nav-wrap swiper-wrapper" >
                             <div className="nav-wrap-item swiper-slide active" >
-                            {
-                                
+                            {                          
                                 (function(){
                                     let lis = [];
                                     for(var i=0;i<navList.length;i++){
@@ -57,7 +61,6 @@ export default class Home extends Component{
                                 
                             }       
                             </div>
-
                             <div className="nav-wrap-item swiper-slide hidden" >
                             {
                                 
@@ -75,10 +78,52 @@ export default class Home extends Component{
                                 })()
                                 
                             } 
-                            </div>
-                             
+                            </div>  
                         </div>
                     </div>
+
+                    {/* 优惠+banner轮播图 */}
+                    <div className="yh-banner" >
+                        
+                        {/* 优惠 */}
+                        <section>
+                            <div className="youhui" >
+                            {
+                                (function(){
+                                    let lis = [];
+                                    for(var i=0;i<yhList.length;i++){
+                                        if(i<2){
+                                           lis.push(
+                                            <div className="youhui-item" >
+                                                <h3 className="name" >{yhList[i].name}</h3>
+                                                <h3 className="description" >{yhList[i].description}</h3>
+                                                <h3 className="qiang" >立即抢购 ></h3>
+                                                <img src={yh_img}/>
+                                            </div> 
+                                           );
+                                        }
+                                    }
+                                    return lis;
+                                }())
+                            }
+                                
+                            </div>
+                        </section>
+                        {/* banner轮播图 */}
+                        <div className="banner swiper-container" >
+                            <div className="banner-wrap swiper-wrapper" >
+                                {
+                                    bannerList.map(item=>{
+                                        return (
+                                            <div className="banner-item swiper-slide" >
+                                                <img src={banner_img}/>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div> 
 
                     <nav>
                         { 
@@ -99,13 +144,25 @@ export default class Home extends Component{
     }
     componentDidMount(){
 
-        //请求nav轮播图
+        //请求nav轮播图  //请求优惠数据
         fetch(API.NAV_API+"?latitude="+31.230378+"&longitude="+121.473658+
         "&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template")
             .then(response=>{
                 return response.json();
             }).then(json=>{
                 this.setState({navList:json[0].entries});
+                this.setState({yhList:json[1].entries})
+                // console.log(json)
+            }).catch(ex=>{
+                console.log('',ex);
+            })
+
+        //请求banner轮播图
+        fetch(API.BANNER_API+"?consumer="+1+"&type="+1+"&latitude="+31.230378+"&longitude="+121.473658)
+            .then(response=>{
+                return response.json();
+            }).then(json=>{
+                this.setState({bannerList:json});
                 // console.log(json)
             }).catch(ex=>{
                 console.log('',ex);
@@ -128,9 +185,19 @@ export default class Home extends Component{
             })
 
 
-        // 导航栏轮播图
-        var navSwiper = new Swiper ('.swiper-container', {
+        
+        
+        
+        
+        // 导航栏nav轮播图
+        var navSwiper = new Swiper ('.nav-container', {
             // loop:true
         })
+
+        // banner轮播图
+        var bannerSwiper = new Swiper ('.banner', {
+            // loop:true
+        })
+
     }
 }
